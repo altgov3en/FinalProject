bea6e9e479cb45b90e5dcdab651d82f56a585cd5
altgov3en <altgov3en@gmail.com>
2015-04-19 15:37:51 +0300

    New Histogram window

---
+++ Image procession and segmentation/HistogramWindow.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Image_procession_and_segmentation
+{
+    public partial class HistogramWindow : Form
+    {
+        public HistogramWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
