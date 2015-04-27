a5d5ed4cf0a8c109b5bdab6617032d7cbafc0a88
altgov3en <altgov3en@gmail.com>
2015-04-27 18:51:10 +0300

    working on new eresion function

--- Image procession and segmentation/ImageController.cs
+++ Image procession and segmentation/ImageController.cs
@@ -71,7 +71,8 @@ namespace Image_procession_and_segmentation
         //"Image Analysis Tools->Erode the Image" event
         private void DilatateTheImageToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            this.DilatateGrayscaledImage();
+            //this.DilatateGrayscaledImage();
+            this.NEWerodeGrayscaledImage();
         }
 
         //"Image Analysis Tools->Dilatate the Image" event
@@ -236,37 +237,72 @@ namespace Image_procession_and_segmentation
         #endregion
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-        public void erodeGrayscaledImage()
+        public void NEWerodeGrayscaledImage()
         {
             BitmapData sourceData = OpenedImageData.openedImageGrayscaled.LockBits(new System.Drawing.Rectangle(0, 0, OpenedImageData.openedImage.Width, OpenedImageData.openedImage.Height), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
             
             OpenedImageData.openedImageEroded = new Bitmap(sourceData.Width, sourceData.Height);
-            BitmapData destData = OpenedImageData.openedImageEroded.LockBits(new System.Drawing.Rectangle(0, 0, OpenedImageData.openedImage.Width-1, OpenedImageData.openedImage.Height-1), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+            BitmapData destData = OpenedImageData.openedImageEroded.LockBits(new System.Drawing.Rectangle(0, 0, sourceData.Width, sourceData.Height), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
          
             unsafe
             {
                 byte* sourcePtr = (byte*)sourceData.Scan0;
                 byte* destPtr = (byte*)destData.Scan0;
 
-                byte redValue, greenValue, blueValue = 0;
+                int blueValue = 0;
+                int greenValue = 0;
+                int redValue = 0;
 
-                int index = sourceData.Width + 1;
-
-                destPtr += sourceData.Width + 1;
- 
-                for (int i = 1; i < sourceData.Height - 1; i++)//for each row
+                for (int i = 0; i < sourceData.Height - 3; i++)//for each row
                 {
-                    for (int j = 1; j < sourceData.Width - 1; j++)//for each column
+                    for (int j = 0; j < sourceData.Width - 3; j++)//for each column
                     {
-                        blueValue += sourcePtr[index - sourceData.Width + 1];
-                        blueValue += sourcePtr[index - sourceData.Width + 1 + 3];
-                        blueValue += sourcePtr[index - sourceData.Width + 1 + 3 + 3];
+                        blueValue += sourcePtr[0];
+                        blueValue += sourcePtr[3];
+                        blueValue += sourcePtr[6];
+                        greenValue += sourcePtr[1];
+                        greenValue += sourcePtr[4];
+                        greenValue += sourcePtr[7];
+                        redValue += sourcePtr[2];
+                        redValue += sourcePtr[5];
+                        redValue += sourcePtr[8];
 
+                        sourcePtr += sourceData.Width;
+                        blueValue += sourcePtr[0];
+                        blueValue += sourcePtr[6];
+                        greenValue += sourcePtr[1];
+                        greenValue += sourcePtr[7];
+                        redValue += sourcePtr[2];
+                        redValue += sourcePtr[8];
 
+                        sourcePtr += sourceData.Width;
+                        blueValue += sourcePtr[0];
+                        blueValue += sourcePtr[3];
+                        blueValue += sourcePtr[6];
+                        greenValue += sourcePtr[1];
+                        greenValue += sourcePtr[4];
+                        greenValue += sourcePtr[7];
+                        redValue += sourcePtr[2];
+                        redValue += sourcePtr[5];
+                        redValue += sourcePtr[8];
 
+                        blueValue /= 9;
+                        greenValue /= 9;
+                        redValue /= 9;
+
+                        destPtr[0] = (byte)blueValue;
+                        destPtr[1] = (byte)greenValue;
+                        destPtr[2] = (byte)redValue;
+
+                        sourcePtr -= 2 * sourceData.Width;
+                        sourcePtr += 3;
+
+                        destPtr += 3;
                     }
                 }
             }
+            OpenedImageData.openedImageEroded.UnlockBits(destData);
+            this.applicationForm.pictureBox1.Image = OpenedImageData.openedImageEroded;
         }
 
         public void calculateHistogram()
